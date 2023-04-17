a089c3e5b93959e67f27a5618e7368fb0853e0ce
Karen Atkinson <karen.atkinson.uk@gmail.com>
2023-04-17 22:54:11 +0100

    Dataclass models for Game, Review and Stocks

---
+++ Karen/flask/application/models/game.py
@@ -0,0 +1,24 @@
+from application import db
+from dataclasses import dataclass
+
+
+@dataclass
+class Game(db.Model):
+
+    game_id: int
+    game_name: str
+    num_of_players: int
+    min_age: int
+    duration_of_play_time: int
+    gameplay: str
+    game_description: str
+
+    game_id = db.Column(db.Integer, primary_key=True)
+    game_name = db.Column(db.String(50), nullable=False)
+    num_of_players = db.Column(db.Integer, nullable=False)
+    min_age = db.Column(db.Integer, nullable=False)
+    duration_of_play_time = db.Column(db.Integer, nullable=False)
+    gameplay = db.Column(db.String(50), nullable=True)
+    game_description = db.Column(db.String(10000), nullable=True)
+    reviews = db.relationship("Review", back_populates="games")
+    stocks = db.relationship("Stock", back_populates="games")
