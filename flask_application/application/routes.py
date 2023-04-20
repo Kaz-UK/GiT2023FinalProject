35644ed3b9295a275fbab9fc5be0bb834fe1abb7
Karen Atkinson <karen.atkinson.uk@gmail.com>
2023-04-20 12:31:02 +0100

    Updated routes and services

--- flask_application/application/routes.py
+++ flask_application/application/routes.py
@@ -7,5 +7,80 @@ from application.models.review import Review
 from application.models.stock import Stock
 
 from application import db
-
 from application import app, service
+
+
+# Creates a homepage using the Jinja template
+@app.route('/')
+@app.route('/home')
+def show_home():
+    return render_template('home.html', title="Welcome")
+
+# Creates an about us page using the Jinja template
+@app.route('/about')
+def show_about():
+    return render_template('about.html', title="About Us")
+
+
+# All customers
+@app.route('/customers', methods=['GET'])
+def show_customers():
+    error = ""
+    customers = service.get_all_customers()
+    if len(customers) == 0:
+        error = "There are no customers to display"
+    #return render_template('customer.html', customers=customers, message=error, title="All Customer's Information")
+    return jsonify(customers)
+
+
+# All games
+@app.route('/games', methods=['GET'])
+def show_games():
+    error = ""
+    games = service.get_all_games()
+    if len(games) == 0:
+        error = "There are no games to display"
+    #return render_template('customer.html', customers=customers, message=error, title="All Games")
+    return jsonify(games)
+
+
+# All reviews
+@app.route('/reviews', methods=['GET'])
+def show_reviews():
+    error = ""
+    reviews = service.get_all_reviews()
+    if len(reviews) == 0:
+        error = "There are no reviews to display"
+    #return render_template('customer.html', reviews=reviews, message=error, title="All Reviews")
+    return jsonify(reviews)
+
+
+# All bookings
+@app.route('/bookings', methods=['GET'])
+def show_bookings():
+    error = ""
+    bookings = service.get_all_bookings()
+    if len(bookings) == 0:
+        error = "There are no bookings to display"
+    #return render_template('customer.html', bookings=bookings, message=error, title="All Bookings")
+    return jsonify(bookings)
+
+# All cafesessions
+@app.route('/cafesessions', methods=['GET'])
+def show_cafesessions():
+    error = ""
+    cafesessions = service.get_all_cafesessions()
+    if len(cafesessions) == 0:
+        error = "There are no cafesessions to display"
+    #return render_template('customer.html', cafesessions=cafesessions, message=error, title="All Sessions")
+    return jsonify(cafesessions)
+
+# All stock
+@app.route('/stock', methods=['GET'])
+def show_stock():
+    error = ""
+    stock = service.get_all_stock()
+    if len(stock) == 0:
+        error = "There is no stock to display"
+    #return render_template('customer.html', stock=stock, message=error, title="All Stocks")
+    return jsonify(stock)
--- flask_application/application/service.py
+++ flask_application/application/service.py
@@ -6,3 +6,33 @@ from application.models.review import Review
 from application.models.stock import Stock
 
 from application import db
+
+# All customers
+def get_all_customers():
+    return db.session.query(Customer).all()
+
+
+# All games
+def get_all_games():
+    return db.session.query(Game).all()
+
+
+# All reviews
+def get_all_reviews():
+    return db.session.query(Review).all()
+
+
+# All bookings
+def get_all_bookings():
+    return db.session.query(Booking).all()
+
+
+# All cafesessions
+def get_all_cafesessions():
+    return db.session.query(Cafesession).all()
+
+
+# All reviews
+def get_all_stocks():
+    return db.session.query(Stock).all()
+
