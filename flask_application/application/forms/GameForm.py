0de222750e95588ed2d1063621cb9a9f9069ac28
Faye <fayequinn88@hotmail.com>
2023-04-30 21:43:04 +0100

    max num of players

--- flask_application/application/forms/GameForm.py
+++ flask_application/application/forms/GameForm.py
@@ -6,7 +6,7 @@ from wtforms.widgets import TextArea
 
 class GameForm(FlaskForm):
     game_name = StringField('Game Name', validators=[DataRequired()])
-    num_of_players = IntegerField('Number of Players', validators=[DataRequired()])
+    num_of_players = IntegerField('Maximum Number of Players', validators=[DataRequired()])
     min_age = IntegerField('Minimum Age', validators=[DataRequired()])
     duration_of_play_time = IntegerField('Duration of Play (min)', validators=[DataRequired()])
     gameplay = SelectField('Game Play', choices=[('competitive', 'competitive'), ('co-operative', 'co-operative')])
